e12e0073aecf75c780854219af8ae6ac522b72f5
MOHAK12309 <yashraj63951@gmail.com>
2023-12-02 21:53:49 +0530

    game

--- src/components/auth/join.js
+++ src/components/auth/join.js
@@ -2,96 +2,66 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
+import ReCAPTCHA from "react-google-recaptcha";
+
 function Join() {
   const [name, setName] = useState("");
-  const [phone, setphoneNumber] = useState("");
+  const [phone, setPhoneNumber] = useState("");
   const [description, setDescription] = useState("");
   const [email, setEmail] = useState("");
-  const Navigate=useNavigate('')
+  const [recaptchaValue, setRecaptchaValue] = useState(null); // New state for reCAPTCHA
+  const Navigate = useNavigate("");
 
-  const handlesubmit = async (e) => {
-  
+  const handleRecaptchaChange = (value) => {
+    setRecaptchaValue(value);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!recaptchaValue) {
+      toast.error("Please verify that you are not a robot.");
+      return;
+    }
+
     try {
-      const res = await axios.post("https://server.ourcadium.com/api/v1/user/signup", {
-        name: name,
-        phoneNumber: phone,
-        desc: description,
-        email: email,
-      });
-      if(res.data.statusbar === "success"){
-        toast.success('Recruitment initiated')
-        Navigate('/end')
-        
+      const res = await axios.post(
+        "https://server.ourcadium.com/api/v1/user/signup",
+        {
+          name: name,
+          phoneNumber: phone,
+          desc: description,
+          email: email,
+        }
+      );
 
-
+      if (res.data.statusbar === "success") {
+        toast.success("Recruitment initiated");
+        Navigate("/end");
       }
 
       console.log(res);
     } catch (error) {
       console.log(error);
-      toast.error('you are already registerd')
+      toast.error("You are already registered");
     }
   };
+
   return (
     <div className="joinback">
-      <div className="join-form">
-        <h4 className="join-font">JOIN THE ARMADA TO BEYOND THE KNOWN</h4>
-        <form onSubmit={handlesubmit} className="join-form-main">
-          <div style={{ width: "80%", margin: "auto" }}>
-          
-              <input
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                className="input-form"
-                type="text"
-                placeholder="Name*"
-              ></input>
-          
-              <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="input-form"
-                type="email"
-                placeholder="Email*"
-              ></input>
-           
-           
-              <input
-                onChange={(e) => setphoneNumber(e.target.value)}
-                value={phone}
-                className="input-form"
-                type="tel"
-                placeholder="Contact*"
-              ></input>
-            
-          
-              <textarea
-                placeholder="Additional Note"
-                onChange={(e) => setDescription(e.target.value)}
-                value={description}
-                className="input-form"
-                style={{ height: "180px" }}
-              ></textarea>
-           
-         <button type="submit"   className="join-btn2">JOIN NOW</button>
-           
-          </div>
-        </form>
-      </div>
-      <div
-        style={{
-          textAlign: "center",
-          width: "100%",
-          margin: "auto",
-          position: "absolute",
-          bottom: "10px",
-        }}
-      >
-        <h3 className="right">ALL RIGHTS RESERVED | © www.OURCADIUM.com</h3>
-      </div>
-      
+      {/* ... your existing code ... */}
+      <form onSubmit={handleSubmit} className="join-form-main">
+        {/* ... your existing input fields ... */}
+        {/* Add reCAPTCHA component */}
+        <ReCAPTCHA
+          sitekey="6Ld7FCQpAAAAAEVxVaBwSAXPjfljYxrfArXTSLDz"
+          onChange={handleRecaptchaChange}
+        />
+        <button type="submit" className="join-btn2">
+          JOIN NOW
+        </button>
+      </form>
     </div>
   );
 }

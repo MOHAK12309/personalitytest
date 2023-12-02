1d2e8790e0e8d46d739fe16bae8c9099d1e78a0b
MOHAK12309 <yashraj63951@gmail.com>
2023-12-02 22:03:44 +0530

    game

--- src/components/auth/join.js
+++ src/components/auth/join.js
@@ -50,18 +50,65 @@ function Join() {
 
   return (
     <div className="joinback">
-      {/* ... your existing code ... */}
-      <form onSubmit={handleSubmit} className="join-form-main">
-        {/* ... your existing input fields ... */}
-        {/* Add reCAPTCHA component */}
-        <ReCAPTCHA
-          sitekey="6Ld7FCQpAAAAAEVxVaBwSAXPjfljYxrfArXTSLDz"
-          onChange={handleRecaptchaChange}
-        />
-        <button type="submit" className="join-btn2">
-          JOIN NOW
-        </button>
-      </form>
+      <div className="join-form">
+        <h4 className="join-font">JOIN THE ARMADA TO BEYOND THE KNOWN</h4>
+        <form onSubmit={handleSubmit} className="join-form-main">
+          <div style={{ width: "80%", margin: "auto" }}>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              className="input-form"
+              type="text"
+              placeholder="Name*"
+            ></input>
+
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input-form"
+              type="email"
+              placeholder="Email*"
+            ></input>
+
+            <input
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              value={phone}
+              className="input-form"
+              type="tel"
+              placeholder="Contact*"
+            ></input>
+
+            <textarea
+              placeholder="Additional Note"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              className="input-form"
+              style={{ height: "180px" }}
+            ></textarea>
+
+            {/* Add reCAPTCHA component */}
+            <ReCAPTCHA
+              sitekey="YOUR_RECAPTCHA_SITE_KEY"
+              onChange={handleRecaptchaChange}
+            />
+
+            <button type="submit" className="join-btn2">
+              JOIN NOW
+            </button>
+          </div>
+        </form>
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          width: "100%",
+          margin: "auto",
+          position: "absolute",
+          bottom: "10px",
+        }}
+      >
+        <h3 className="right">ALL RIGHTS RESERVED | © www.OURCADIUM.com</h3>
+      </div>
     </div>
   );
 }

92628cb58878dea17a51d9c0253c263df0025fd1
MOHAK12309 <yashraj63951@gmail.com>
2023-12-05 15:45:06 +0530

    completed

--- src/components/auth/join.js
+++ src/components/auth/join.js
@@ -1,32 +1,28 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 import ReCAPTCHA from "react-google-recaptcha";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+
 function Join() {
   const [name, setName] = useState("");
   const [phone, setPhoneNumber] = useState("");
   const [description, setDescription] = useState("");
   const [email, setEmail] = useState("");
-  const [recaptchaValue, setRecaptchaValue] = useState(null); // New state for reCAPTCHA
-  const Navigate = useNavigate("");
+  const navigate = useNavigate();
 
-  const handleRecaptchaChange = (value) => {
-    setRecaptchaValue(value);
-  };
+  const recaptchaRef = React.createRef(); // Create a ref for the reCAPTCHA component
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!recaptchaValue) {
-      toast.error("Please verify that you are not a robot.");
-      return;
-    }
-
     try {
+      // Execute reCAPTCHA programmatically
+      const recaptchaValue = await recaptchaRef.current.executeAsync();
+
       const res = await axios.post(
         "https://server.ourcadium.com/api/v1/user/signup",
         {
@@ -34,12 +30,13 @@ function Join() {
           phoneNumber: phone,
           desc: description,
           email: email,
+          recaptchaValue: recaptchaValue,
         }
       );
 
       if (res.data.statusbar === "success") {
         toast.success("Recruitment initiated");
-        Navigate("/end");
+        navigate("/end");
       }
 
       console.log(res);
@@ -53,7 +50,7 @@ function Join() {
     <div className="joinback">
       <div className="join-form">
         <h4 className="join-font">JOIN THE ARMADA TO BEYOND THE KNOWN</h4>
-    
+
         <div className="join-form-main">
           <div style={{ width: "80%", margin: "auto" }}>
             <input
@@ -88,25 +85,24 @@ function Join() {
               style={{ height: "180px" }}
             ></textarea>
 
-            {/* Add reCAPTCHA component */}
-      
+            {/* Add invisible reCAPTCHA component */}
             <ReCAPTCHA
-                        
-
-              style={{width:"100px"}}
+              ref={recaptchaRef} // Attach the ref to the reCAPTCHA component
               sitekey="6Ld7FCQpAAAAAEVxVaBwSAXPjfljYxrfArXTSLDz"
-              onChange={handleRecaptchaChange}
+              size="invisible"
             />
-          
-            <div style={{width:"80%", textAlign:"left"}}>
-            <Button type="submit" onClick={handleSubmit} style={{background:"#0d4f74"}} variant="contained"className="join-btn2">
-            JOIN NOW
-            </Button>
 
-
+            <div style={{ width: "80%", textAlign: "left" }}>
+              <Button
+                type="submit"
+                onClick={handleSubmit}
+                style={{ background: "#0d4f74" }}
+                variant="contained"
+                className="join-btn2"
+              >
+                JOIN NOW
+              </Button>
             </div>
-
-          
           </div>
         </div>
       </div>
